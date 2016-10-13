782aa51b7f5196b8ab9da30fcf0eb5c90dd737a6
manimotapothula <mani.motapothula@gmail.com>
2016-10-13 20:48:31 +0530

    c language

---
+++ positive (or) negative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("enter the value of i");
+scanf("%d",&i);
+if(i>=0)
+{
+printf("the value is positive");
+}
+else
+{
+printf("the value is negative");
+}
+return 0;
+} 
